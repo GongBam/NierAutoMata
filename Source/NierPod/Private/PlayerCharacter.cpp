a66f8cfa70aafc8487870b7e0343901f89331970
KSW1999 <kso8242@naver.com>
2024-04-17 21:34:11 +0900

    아담피 깎는 함수 플레이어에 추가됨
    Q 누르면 아담 피 50 씩 깎임

--- Source/NierPod/Private/PlayerCharacter.cpp
+++ Source/NierPod/Private/PlayerCharacter.cpp
@@ -83,10 +83,10 @@ void APlayerCharacter::BeginPlay()
     playerAnim = Cast<UTEST2BAnimInstance>(GetMesh()->GetAnimInstance());
     weaponComp->OnComponentBeginOverlap.AddDynamic(this, &APlayerCharacter::OnOverlap);
 
-
+    // 체력바 UI 위젯 캐싱 
     playerUI = Cast<UPlayerHealthWidget>(playerwidgetComp->GetWidget());
 
-    //체력바 UI 위젯 
+    //체력바 UI 위젯 붙임 
     if (playerHealthWidget_bp != nullptr)
     {
         playerUI = CreateWidget<UPlayerHealthWidget>(GetWorld(), playerHealthWidget_bp);
@@ -107,7 +107,22 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
     Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+<<<<<<< Updated upstream
     UEnhancedInputComponent* enhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+=======
+    if (UEnhancedInputComponent* Input = CastChecked <UEnhancedInputComponent>(PlayerInputComponent))
+    {   //인풋-함수 바인드
+        Input->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+        Input->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+        Input->BindAction(JumpAction, ETriggerEvent::Started, this, &APlayerCharacter::Jump);
+        Input->BindAction(DashAction, ETriggerEvent::Started, this, &APlayerCharacter::Dash);
+        Input->BindAction(DodgeAction, ETriggerEvent::Started, this, &APlayerCharacter::DodgeFunction);
+        Input->BindAction(LeftAttackAction, ETriggerEvent::Started, this, &APlayerCharacter::LeftAttack);
+        Input->BindAction(RightAttackAction, ETriggerEvent::Started, this, &APlayerCharacter::RightAttack);
+        Input->BindAction(shooting, ETriggerEvent::Triggered, this, &APlayerCharacter::Shot);
+        Input->BindAction(damaging, ETriggerEvent::Started, this, &APlayerCharacter::DAMAGING);
+    }
+>>>>>>> Stashed changes
 
     if(enhancedInputComponent != nullptr)
     {
@@ -130,7 +145,12 @@ void APlayerCharacter::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActo
     }
 }
 
+<<<<<<< Updated upstream
 void APlayerCharacter::PlayerMove(const FInputActionValue& Value)
+=======
+//이동
+void APlayerCharacter::Move(const FInputActionValue& InputValue)
+>>>>>>> Stashed changes
 {
     FVector2D inputValue = Value.Get<FVector2D>();
     moveDirection = FVector(inputValue.Y, inputValue.X, 0);
@@ -193,6 +213,120 @@ void APlayerCharacter::Shot(const FInputActionValue& InputValue)
 }
 
 
+<<<<<<< Updated upstream
+=======
+void APlayerCharacter::SetActionState(EStateType eState)
+{
+    if (eActionState != eState)
+    {
+        eActionState = eState;
+    }
+}
+
+void APlayerCharacter::PerformDodge()
+{
+    SetActionState(EStateType::Nothing);
+
+    if (DodgeMontage != nullptr)
+    {
+        PlayAnimMontage(DodgeMontage);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("DodgeMontage Is Null"));
+    }
+}
+
+// 임시로 보스 데미지주는 함수 
+void APlayerCharacter::DAMAGING(const FInputActionValue& InputValue)
+{
+   
+    for(TActorIterator<ABossCharacter> iter(GetWorld()); iter; ++iter)
+    {
+        bossCharacter = *iter;
+      if (bossCharacter != nullptr)
+      {   
+          bossCharacter->OnDamaged(damage + 50);
+      }
+    }
+}
+
+// PerformLightAttack
+bool APlayerCharacter::PerformLightAttack(int32 attackIndex)
+{
+    UAnimMontage* selectedLightAttackMontage = lightAttackMontages[attackIndex];
+
+    if (IsValid(selectedLightAttackMontage))
+    {   
+        SetActionState(EStateType::Attack);
+
+        PlayAnimMontage(selectedLightAttackMontage);
+
+        LightAttackIndex++;
+
+
+        if (LightAttackIndex >= lightAttackMontages.Num())
+        {
+            LightAttackIndex = 0;
+        }
+
+        return true;
+    }
+    else
+    {
+        FString strMessage = FString(TEXT("Attack Montage Not Valid"));
+        UKismetSystemLibrary::PrintString(GetWorld(), *strMessage);
+
+        return false;
+    }
+
+    return false;
+}
+
+// Reset Light Attack Variables
+void APlayerCharacter::ResetLightAttackVariables()
+{
+    LightAttackIndex = 0;
+    LightAttackSaved = false;
+
+
+}
+
+bool APlayerCharacter::PerformHeavyAttack(int32 attackIndex)
+{
+    UAnimMontage* selectedHeavyAttackMontage = heavyAttackMontages[attackIndex];
+
+    if (IsValid(selectedHeavyAttackMontage)) {
+     
+        SetActionState(EStateType::Attack);
+
+        PlayAnimMontage(selectedHeavyAttackMontage);
+
+        HeavyAttackIndex++;
+
+
+        if (HeavyAttackIndex >= heavyAttackMontages.Num()) {
+            HeavyAttackIndex = 0;
+        }
+        return true;
+    }
+    else {
+        FString strMessage = FString(TEXT("Attack Montage Not Valid"));
+        UKismetSystemLibrary::PrintString(GetWorld(), *strMessage);
+
+        return false;
+    }
+    return false;
+}
+
+void APlayerCharacter::ResetHeavyAttackVariables()
+{
+    HeavyAttackIndex = 0;
+    HeavyAttackSaved = false;
+
+}
+
+>>>>>>> Stashed changes
 void APlayerCharacter::PlayerDamaged()
 {
     currentHP = FMath::Clamp(currentHP - 10, 0, maxHP);
@@ -209,9 +343,30 @@ void APlayerCharacter::PlayerDamaged()
     {
         PlayerDie();
     }
+<<<<<<< Updated upstream
 
  //   UE_LOG(LogTemp, Warning, TEXT("Player HP : %d"), currentHP);
  }
+=======
+}
+
+void APlayerCharacter::LeftAttack(const FInputActionValue& InputValue)
+{
+    UE_LOG(LogTemp, Warning, TEXT("LeftAttack"));
+
+    PerformLightAttack(LightAttackIndex);
+
+}
+
+void APlayerCharacter::RightAttack(const FInputActionValue& InputValue)
+{
+    UE_LOG(LogTemp, Warning, TEXT("RightAttack"));
+
+    PerformHeavyAttack(HeavyAttackIndex);
+
+}
+
+>>>>>>> Stashed changes
 
 //플레이어 죽는 함수 
 void APlayerCharacter::PlayerDie()
--- Source/NierPod/Public/PlayerCharacter.h
+++ Source/NierPod/Public/PlayerCharacter.h
@@ -59,6 +59,8 @@ public:
 	UPROPERTY(EditAnywhere, Category = "PlayerSettings")
 	TSubclassOf<class UPlayerHealthWidget> playerHealthWidget_bp;
 
+	class ABossCharacter* bossCharacter;
+	
 
 protected:
 
@@ -66,6 +68,7 @@ protected:
 
 
 	//이동, 시야, 점프 함수 
+<<<<<<< Updated upstream
 	UFUNCTION()
 	void PlayerMove(const FInputActionValue& Value);
 	UFUNCTION()
@@ -77,6 +80,36 @@ protected:
 	UFUNCTION()
 	void Shot(const FInputActionValue& Value);
 	UFUNCTION()
+=======
+	void Move(const FInputActionValue& InputValue);
+	void Look(const FInputActionValue& InputValue);
+	void Jump();
+
+	//공격 함수
+	void Dash(const FInputActionValue& InputValue);
+	void DodgeFunction(const FInputActionValue& InputValue);
+	void Shot(const FInputActionValue& InputValue);
+	
+	//void ResetDoubleJump();
+
+	void SetActionState(EStateType eState);
+
+	// Dodge
+	void PerformDodge();
+	void SaveDodge();
+
+	//임시로 보스 데미지주는 함수
+	void DAMAGING(const FInputActionValue& InputValue);
+
+	// LightAttack
+	bool PerformLightAttack(int32 attackIndex);
+	void ResetLightAttackVariables();
+
+	// HeavyAttack
+	bool PerformHeavyAttack(int32 attackIndex);
+	void ResetHeavyAttackVariables();
+
+>>>>>>> Stashed changes
 	void PlayerDie();
 
 
@@ -90,4 +123,80 @@ protected:
 	class APlayerController* pc;
 	class UTEST2BAnimInstance* playerAnim;
 
+<<<<<<< Updated upstream
+=======
+	//인풋액션
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* AttackAction;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* LeftAttackAction;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* RightAttackAction;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* shooting;
+
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* damaging;
+
+
+
+	// 대쉬
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* DashAction;
+
+	// 닷지
+	UPROPERTY(EditAnywhere, Category = "EnhancedInput")
+	class UInputAction* DodgeAction;
+
+	class UAnimInstance* AnimInstance;
+
+	bool canDash = true;
+	FVector OldVelocity;
+	FVector Velocity;
+	float DashCooldown = 0.2f;
+
+	int32 damage = 10;
+
+	// 이단 점프
+	bool DoubleJump;
+
+	bool bSaveDodge;
+
+	// 왼쪽 공격
+	EStateType eActionState;
+	int32 LightAttackIndex = 0;
+	bool LightAttackSaved;
+
+	// 오른쪽 공격
+	int32 HeavyAttackIndex = 0;
+	bool HeavyAttackSaved;
+
+	// Dash 몽타주 슬롯 생성
+	UPROPERTY(EditAnywhere, Category = "Montage")
+	class UAnimMontage* DashMontage;
+
+	// Dodge 몽타주 슬롯 생성
+	UPROPERTY(EditAnywhere, Category = "Montage")
+	class UAnimMontage* DodgeMontage;
+
+	// Light Attack Combo 몽타주 배열 생성
+	UPROPERTY(EditAnywhere, Category = "Montage")
+	TArray<class UAnimMontage*> lightAttackMontages;
+
+	// Heavy Attack Combo 몽타주 배열 생성
+	UPROPERTY(EditAnywhere, Category = "Montage")
+	TArray<class UAnimMontage*> heavyAttackMontages;
+>>>>>>> Stashed changes
 };
